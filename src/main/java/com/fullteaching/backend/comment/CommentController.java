170ed331ed242c23cfcf04eb13bd746711101542
pabloFuente <pablofuenteperez@gmail.com>
2018-02-12 10:14:57 +0100

    bug1

--- src/main/java/com/fullteaching/backend/comment/CommentController.java
+++ src/main/java/com/fullteaching/backend/comment/CommentController.java
@@ -11,9 +11,6 @@ import org.springframework.web.bind.annotation.RestController;
 
 import com.fullteaching.backend.entry.Entry;
 import com.fullteaching.backend.entry.EntryRepository;
-import com.fullteaching.backend.security.AuthorizationService;
-import com.fullteaching.backend.coursedetails.CourseDetails;
-import com.fullteaching.backend.coursedetails.CourseDetailsRepository;
 import com.fullteaching.backend.user.User;
 import com.fullteaching.backend.user.UserComponent;
 
@@ -28,13 +25,7 @@ public class CommentController {
 	private CommentRepository commentRepository;
 	
 	@Autowired
-	private CourseDetailsRepository courseDetailsRepository;
-	
-	@Autowired
 	private UserComponent user;
-	
-	@Autowired
-	private AuthorizationService authorizationService;
 	
 	@RequestMapping(value = "/entry/{entryId}/forum/{courseDetailsId}", method = RequestMethod.POST)
 	public ResponseEntity<Object> newComment(
@@ -43,62 +34,47 @@ public class CommentController {
 			@PathVariable(value="courseDetailsId") String courseDetailsId
 	) {
 		
-		ResponseEntity<Object> authorized = authorizationService.checkBackendLogged();
-		if (authorized != null){
-			return authorized;
-		};
-		
 		long id_entry = -1;
-		long id_courseDetails = -1;
 		try {
 			id_entry = Long.parseLong(entryId);
-			id_courseDetails = Long.parseLong(courseDetailsId);
 		} catch(NumberFormatException e){
 			return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
 		}
+				
+		//Setting the author of the comment
+		User userLogged = user.getLoggedUser();
+		comment.setUser(userLogged);
+		//Setting the date of the comment
+		comment.setDate(System.currentTimeMillis());
 		
-		CourseDetails cd = courseDetailsRepository.findOne(id_courseDetails);
-		
-		ResponseEntity<Object> userAuthorized = authorizationService.checkAuthorizationUsers(cd, cd.getCourse().getAttenders());
-		if (userAuthorized != null) { // If the user is not an attender of the course
-			return userAuthorized;
-		} else {
-		
-			//Setting the author of the comment
-			User userLogged = user.getLoggedUser();
-			comment.setUser(userLogged);
-			//Setting the date of the comment
-			comment.setDate(System.currentTimeMillis());
-			
-			//The comment is a root comment
-			if (comment.getCommentParent() == null){
-				Entry entry = entryRepository.findOne(id_entry);
-				if(entry != null){
-					entry.getComments().add(comment);
-					/*Saving the modified entry: Cascade relationship between entry and comments
-					  will add the new comment to CommentRepository*/
-					entryRepository.save(entry);
-					/*Entire entry is returned*/
-					return new ResponseEntity<>(entry, HttpStatus.CREATED);
-				}else{
-					return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
-				}
+		//The comment is a root comment
+		if (comment.getCommentParent() == null){
+			Entry entry = entryRepository.findOne(id_entry);
+			if(entry != null){
+				entry.getComments().add(comment);
+				/*Saving the modified entry: Cascade relationship between entry and comments
+				  will add the new comment to CommentRepository*/
+				entryRepository.save(entry);
+				/*Entire entry is returned*/
+				return new ResponseEntity<>(entry, HttpStatus.CREATED);
+			}else{
+				return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
 			}
-			
-			//The comment is a replay to another existing comment
-			else{
-				Comment cParent = commentRepository.findOne(comment.getCommentParent().getId());
-				if(cParent != null){
-					cParent.getReplies().add(comment);
-					/*Saving the modified parent comment: Cascade relationship between comment and 
-					 its replies will add the new comment to CommentRepository*/
-					commentRepository.save(cParent);
-					Entry entry = entryRepository.findOne(id_entry);
-					/*Entire entry is returned*/
-					return new ResponseEntity<>(entry, HttpStatus.CREATED);
-				}else{
-					return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
-				}
+		}
+		
+		// The comment is a replay to another existing comment
+		else{
+			Comment cParent = commentRepository.findOne(comment.getCommentParent().getId());
+			if(cParent != null){
+				cParent.getReplies().add(comment);
+				/*Saving the modified parent comment: Cascade relationship between comment and 
+				 its replies will add the new comment to CommentRepository*/
+				commentRepository.save(cParent);
+				Entry entry = entryRepository.findOne(id_entry);
+				/*Entire entry is returned*/
+				return new ResponseEntity<>(entry, HttpStatus.CREATED);
+			} else {
+				return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
 			}
 		}
 	}
