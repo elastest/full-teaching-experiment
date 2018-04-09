67924d6acf94e22d4af4c8c26fce90f42d9fc55f
pabloFuente <pablofuenteperez@gmail.com>
2018-04-09 14:45:31 +0200

    Script for gathering logs of e2e tests updated

--- docker-compose/full-teaching-env/run_tests.sh
+++ docker-compose/full-teaching-env/run_tests.sh
@@ -2,13 +2,18 @@
 
 for NUMB in 1 2 3 4 5
 do
+	# New folder for bug	
+	mkdir bug${NUMB}
+
 	export BUG_TAG=bug$NUMB
 	declare -a arr=("REST" "Chat" "VideoSession")
 	for testName in "${arr[@]}"
 	do
+		# Clean and run docker-compose
    		docker rm -f $(docker ps -a -q) || true
-		docker-compose up --no-color &> bug${NUMB}_SuT_${testName}.txt &
+		docker-compose up --no-color &> bug${NUMB}/bug${NUMB}_SuT_${testName}.txt &
 		
+		# Wait for application
 		while true
 		do
 			HTTP_STATUS=$(curl -Ik https://localhost:5000 | head -n1 | awk '{print $2}')
@@ -17,10 +22,15 @@ do
 			fi
 			sleep 1
 		done
-
+	
+		# Run test
 		cd ../../
-		mvn -Dtest=FullTeachingTestE2E${testName} -B test >> docker-compose/full-teaching-env/bug${NUMB}_Test_${testName}.txt
+		mvn -Dtest=FullTeachingTestE2E${testName} -B test >> docker-compose/full-teaching-env/bug${NUMB}/bug${NUMB}_Test_${testName}.txt
 		cd docker-compose/full-teaching-env
 	done
+	
+	# Copy surefire-reports folder and clean it for next iteration
+	cp -r ../../target/surefire-reports bug${NUMB}/surefire-reports
+	rm -rf ../../target/surefire-reports/*
 done
 
