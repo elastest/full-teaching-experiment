a0261a1c723ed8dcfa9ca7dd10073077d737e8dd
franciscoRdiaz <francisco.diazbayon@gmail.com>
2019-07-31 14:42:15 +0200

    Update script

--- k8s/addSutPrefix.sh
+++ k8s/addSutPrefix.sh
@@ -1,3 +1,3 @@
 #/!bin/bash
 # Add sut prefix for the current execution 
-sed -i -e "s/image: codeurjc/full-teaching/image: codeurjc/full-teaching:${BUG_TAG}/g" -e "s/name: openvidu-java-sut/name: ${ET_SUT_CONTAINER_NAME}-openvidu-java-sut/g" -e "s/io.elastest.service: openvidu-java-sut/io.elastest.service: ${ET_SUT_CONTAINER_NAME}-openvidu-java-sut/g" -e 's/sut_/sut-/g' openvidu-deployment.yaml
+sed -i -e "s/image: codeurjc/full-teaching/image: codeurjc/full-teaching:${BUG_TAG}/g" -e "s/name: full-teaching/name: ${ET_SUT_CONTAINER_NAME}-full-teaching/g" -e "s/io.elastest.service: full-teaching/io.elastest.service: ${ET_SUT_CONTAINER_NAME}-full-teaching/g" -e 's/sut_/sut-/g' full-teaching-deployment.yaml
