f5a1dc97dbe7ea457d85c04dd4e2dc56ff27c1f1
OscarSotoSanchez <oscarsotosanchez@gmail.com>
2019-08-22 17:26:23 +0200

    change waiter

--- src/test/java/com/fullteaching/backend/e2e/BrowserUser.java
+++ src/test/java/com/fullteaching/backend/e2e/BrowserUser.java
@@ -49,8 +49,10 @@ public class BrowserUser {
 	}
 	
 	public void waitUntil(ExpectedCondition<?> condition, String errorMessage) {
+		WebDriverWait waiter = new WebDriverWait(driver, 40);
+
 		try {
-			this.waiter.until(condition);
+			waiter.until(condition);
 		} catch(org.openqa.selenium.TimeoutException timeout) {
 			log.error(errorMessage);
 			throw new org.openqa.selenium.TimeoutException("\"" + errorMessage + "\" (checked with condition) > " + timeout.getMessage());
