c493539136933bbe45f89453b70207fc0097f19e
OscarSotoSanchez <oscarsotosanchez@gmail.com>
2019-08-22 15:08:43 +0200

    waits

--- src/test/java/com/fullteaching/backend/e2e/FullTeachingTestE2EREST.java
+++ src/test/java/com/fullteaching/backend/e2e/FullTeachingTestE2EREST.java
@@ -584,16 +584,12 @@ public class FullTeachingTestE2EREST extends FullTeachingTestE2E {
 
 		openDialog("#add-course-icon", user);
 
-		waitSeconds(1); // Sleep for it to work in elastest
-
 		user.waitUntil(ExpectedConditions.elementToBeClickable(By.id(("input-post-course-name"))),
 				"Input for course name not clickable");
 		user.getDriver().findElement(By.id("input-post-course-name")).sendKeys(courseName);
 		user.getDriver().findElement(By.id("submit-post-course-btn")).click();
 
 		waitForDialogClosed("course-modal", "Addition of course failed", user);
-
-		waitSeconds(1); // Sleep for it to work in elastest
 
 		user.waitUntil(ExpectedConditions.numberOfElementsToBe(By.cssSelector("#course-list .course-list-item"),
 				numberOfCourses + 1), "Unexpected number of courses");
