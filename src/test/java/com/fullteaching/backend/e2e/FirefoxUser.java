c745d8590ee0e42ce67c8ddf47988fbd1cccc1c0
edujgurjc <26252085+EduJGURJC@users.noreply.github.com>
2019-10-23 11:00:19 +0200

    testName capability added

--- src/test/java/com/fullteaching/backend/e2e/FirefoxUser.java
+++ src/test/java/com/fullteaching/backend/e2e/FirefoxUser.java
@@ -29,7 +29,7 @@ import org.openqa.selenium.remote.RemoteWebDriver;
 
 public class FirefoxUser extends BrowserUser {
 
-	public FirefoxUser(String userName, int timeOfWaitInSeconds, String browserId, String userIdentifier) {
+	public FirefoxUser(String userName, int timeOfWaitInSeconds, String testName, String userIdentifier) {
 		super(userName, timeOfWaitInSeconds);
 		
 		FirefoxProfile profile = new FirefoxProfile();
@@ -49,8 +49,15 @@ public class FirefoxUser extends BrowserUser {
 			this.driver = new FirefoxDriver(capabilities);
 		} else {
 			try {
-				capabilities.setCapability("browserId", browserId + "_" + userIdentifier);
-				RemoteWebDriver remote = new RemoteWebDriver(new URL(eusApiURL),  capabilities);
+			    String browserVersion = System.getProperty("browserVersion");
+                if (browserVersion != null) {
+                    capabilities.setVersion(browserVersion);
+                }
+
+                capabilities.setCapability("testName",
+                        testName + "_" + userIdentifier);
+                
+                RemoteWebDriver remote = new RemoteWebDriver(new URL(eusApiURL),  capabilities);
 				remote.setFileDetector(new LocalFileDetector());
 				this.driver = remote;
 			} catch (MalformedURLException e) {
