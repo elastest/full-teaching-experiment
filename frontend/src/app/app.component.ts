c6b25f72f882eb9d816b03c4a1030728c5f4af31
daniel097541 <32720562+daniel097541@users.noreply.github.com>
2020-02-22 23:19:35 +0100

    Angular-Refactor - Added animated background

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -1,5 +1,6 @@
-import {Component} from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 import {Router} from '@angular/router';
+declare var VANTA;
 
 @Component({
   selector: 'app',
@@ -7,12 +8,28 @@ import {Router} from '@angular/router';
   styleUrls: ['./app.component.css'],
 })
 
-export class AppComponent {
+export class AppComponent implements OnInit{
 
   constructor(private router: Router){}
   isVideoSessionUrl(){
 
     return (this.router.url.substring(0, '/session/'.length) === '/session/');
+  }
+
+  ngOnInit(): void {
+    VANTA.NET({
+      el: "#sticky-footer-div",
+      mouseControls: true,
+      touchControls: true,
+      minHeight: 200.00,
+      minWidth: 200.00,
+      scale: 1.00,
+      scaleMobile: 1.00,
+      backgroundColor: 0xc191b,
+      points: 11.00,
+      maxDistance: 15.00,
+      spacing: 17.00
+    })
   }
 
 }
