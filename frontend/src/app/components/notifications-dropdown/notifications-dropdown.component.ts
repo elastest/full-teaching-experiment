476d2dce88b1249dcb20af6740bcfd4dab74d98f
daniel097541 <32720562+daniel097541@users.noreply.github.com>
2020-06-14 14:22:33 +0200

    Fixed small things

---
+++ frontend/src/app/components/notifications-dropdown/notifications-dropdown.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-notifications-dropdown',
+  templateUrl: './notifications-dropdown.component.html',
+  styleUrls: ['./notifications-dropdown.component.css']
+})
+export class NotificationsDropdownComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
